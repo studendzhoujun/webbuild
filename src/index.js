0d55e8e1de67b58e88b80aad91832ad627740bd2
zhoujun <zhoujun@gomeplus.com>
2017-10-20 13:47:17 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: zhouJun 
+ * @Date: 2017-10-20 11:12:51 
+ * @Last Modified by: zhouJun
+ * @Last Modified time: 2017-10-20 13:45:40
+ */
+
+ console.log('====hello====')
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+// You can install more packages below to config more as you like:
+// eslint
+// babel-eslint
+// eslint-config-standard
+// eslint-loader
+// eslint-plugin-html
+// eslint-plugin-promise
+// eslint-plugin-standard
+// postcss-cssnext
+
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+// var bannerPlugin = new webpack.BannerPlugin(
+//   '// { "framework": "Vue" }\n',
+//   { raw: true }
+// )
+module.exports={
+    entry: {
+      app: path.resolve('./src/index.js'),
+    },
+    output: {
+      path: 'dist',
+      filename:'[name].web.js'
+    },
+    module: {
+      // // You can use ESLint now!
+      // // Please:
+      // // 1. npm install {
+      // //   babel-eslint
+      // //   eslint
+      // //   eslint-config-standard
+      // //   eslint-loader
+      // //   eslint-plugin-html
+      // //   eslint-plugin-promise
+      // // } --save-dev
+      // // 2. set .eslintrc
+      // //   take { "extends": "standard" } for example
+      // //   so you need: npm install eslint-plugin-standard --save-dev
+      // // 3. set the config below
+      // preLoaders: [
+      //   {
+      //     test: /\.vue$/,
+      //     loader: 'eslint',
+      //     exclude: /node_modules/
+      //   },
+      //   {
+      //     test: /\.js$/,
+      //     loader: 'eslint',
+      //     exclude: /node_modules/
+      //   }
+      // ],
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }, {
+          test: /\.vue(\?[^?]+)?$/,
+          loaders: ['vue']
+        },{
+          test: /\.less$/,
+          loaders: ['less-loader']
+        }
+      ]
+    },
+    plugins: [
+      // bannerPlugin
+    ]
+  }
+
+
